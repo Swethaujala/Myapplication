c06a33e771d771a12bdb6e3652333ad3143d03fb
Swethaujala <76894936+Swethaujala@users.noreply.github.com>
2021-01-03 11:40:02 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/utils/constants.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.utils
+
+import com.example.myapplication.R
+
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+
+val DEFAULT_ICONS =listOf(
+        R.drawable.ic_ball,
+        R.drawable.ic_camera,
+        R.drawable.ic_cloud,
+        R.drawable.ic_face,
+        R.drawable.ic_fire,
+        R.drawable.ic_flight,
+        R.drawable.ic_flower,
+        R.drawable.ic_love,
+        R.drawable.ic_shop,
+        R.drawable.ic_spa,
+        R.drawable.ic_star,
+        R.drawable.ic_toy
+
+)
